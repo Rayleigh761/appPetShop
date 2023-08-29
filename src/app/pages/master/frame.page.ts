ce806d8adcb243652a40d1232c2b5d5e5a8a1a2c
David gomes <david.paulita@gmail.com>
2023-08-28 21:47:31 -0300

    Implementação de router

--- src/app/pages/master/frame.page.ts
+++ src/app/pages/master/frame.page.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-frame-page',
   template: '<app-navbar></app-navbar><router-outlet></router-outlet>'
 })
 export class FramePageComponent {
-  title = 'petshop';
+
 }
 
